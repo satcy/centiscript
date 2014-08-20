861f21b69b96e29f854d45f6c2f02f40b7eb2bb8
satcy <satoshi.horii@gmail.com>
2014-08-20 18:19:16 +0900

    0.2.2: sortNum(), drawMe(), lineTo(), moveTo()

--- js/core.js
+++ js/core.js
@@ -51,7 +51,6 @@ Centi.prototype.init = function(canvas){
 
 
 
-
 Centi.prototype.parse = function(tw){
     
     tw = tw.replace(/\s/g, "");
@@ -75,6 +74,7 @@ Centi.prototype.parse = function(tw){
     frameMethod = frameMethod.slice(frameMethod.indexOf("{")+1, frameMethod.lastIndexOf("}"));
 
     var forReg = new RegExp(this.name+".for\\(", "g");
+    var ifReg = new RegExp(this.name+".if\\(", "g");
     var numbersReg = new RegExp("(" + this.name + ")\.([0-9\.]+)", "g");
     //var valueReg = /(^[A-Za-z_][A-Za-z0-9_]+)([\%\&\:\(\)\{\}\;\=\+\-\<\>\*\/\[\]\,])/g;
     var valueReg2 = /([A-Za-z0-9_]+)([\%\&\:\(\)\{\}\;\=\+\-\<\>\*\/\[\]\,\^])/g;
@@ -83,6 +83,7 @@ Centi.prototype.parse = function(tw){
     //setupMethod = setupMethod.replace(new RegExp(this.name + "." + this.name, "g"), this.name);
     setupMethod = setupMethod.replace(numbersReg, "$2");
     setupMethod = setupMethod.replace(forReg, "for(");
+    setupMethod = setupMethod.replace(ifReg, "if(");
     for ( var i=0; i<MATHS.length; i++ ) {
         var math_word = this.name + "." + MATHS[i] + "\\(";
         setupMethod = setupMethod.replace(new RegExp(math_word, "g"), "Math." + MATHS[i] + "(");
@@ -97,6 +98,7 @@ Centi.prototype.parse = function(tw){
     //frameMethod = frameMethod.replace(new RegExp(this.name + "." + this.name, "g"), this.name);
     frameMethod = frameMethod.replace(numbersReg, "$2");
     frameMethod = frameMethod.replace(forReg, "for(");
+    frameMethod = frameMethod.replace(ifReg, "if(");
     for ( var i=0; i<MATHS.length; i++ ) {
         var math_word = this.name + "." + MATHS[i] + "\\(";
         frameMethod = frameMethod.replace(new RegExp(math_word, "g"), "Math." + MATHS[i] + "(");
@@ -113,7 +115,7 @@ Centi.prototype.parse = function(tw){
     evalInContext(setupMethod, this);
     return true;
 
-}
+};
 
 Centi.prototype.update = function(){
     evalInContext(this.drawMethod, this);
@@ -135,13 +137,13 @@ Centi.prototype.bg = function(){
         this.bgcolor.b = parseInt(arguments[2]);     
     }
     this.clear();
-}
+};
 
 Centi.prototype.lg = function(){
     console.log(arguments);
-}
+};
 
-Centi.prototype.sz = function(_w, _h){ this.size(_w, _h); }
+Centi.prototype.sz = function(_w, _h){ this.size(_w, _h); };
 Centi.prototype.size = function(_w, _h){
     this.w = parseInt(_w);
     this.h = parseInt(_h);
@@ -149,13 +151,13 @@ Centi.prototype.size = function(_w, _h){
     this.canvas.height = this.h;
     this.cx = this.w/2;
     this.cy = this.h/2;
-}
+};
 
-Centi.prototype.clr = function(){ this.clear(); }
+Centi.prototype.clr = function(){ this.clear(); };
 Centi.prototype.clear = function(){
     this.ctx.fillStyle = "rgb("+this.bgcolor.r+","+this.bgcolor.g+","+this.bgcolor.b+")";
     this.ctx.fillRect(0,0,this.w, this.h);    
-}
+};
 
 // Randomize
 
@@ -168,7 +170,7 @@ Centi.prototype.rnd = function(){
     } else {
         return Math.random();
     }
-}
+};
 Centi.prototype.rand = function(){
     var len = arguments.length;
     if ( len == 1 ) {
@@ -178,7 +180,7 @@ Centi.prototype.rand = function(){
     } else {
         return Math.random();
     }
-}
+};
 
 Centi.prototype.nz = function() {
     var len = arguments.length;
@@ -190,7 +192,7 @@ Centi.prototype.nz = function() {
         return perlin.perlin3(arguments[0], arguments[1], arguments[2]);   
     }
     return 0; 
-}
+};
 Centi.prototype.noise = function(){
     var len = arguments.length;
     if ( len == 1 ) {
@@ -201,29 +203,57 @@ Centi.prototype.noise = function(){
         return perlin.perlin3(arguments[0], arguments[1], arguments[2]);   
     }
     return 0;
-}
+};
 
 // draw
 
 Centi.prototype.rect = function(_x, _y, _w, _h){
     if ( this.bFill ) this.ctx.fillRect(_x, _y, _w, _h);
     else this.ctx.strokeRect(_x, _y, _w, _h);
-}
+};
 
-Centi.prototype.ln = function(_x1, _y1, _x2, _y2){ this.line(_x1, _y1, _x2, _y2); }
+Centi.prototype.ln = function(_x1, _y1, _x2, _y2){ this.line(_x1, _y1, _x2, _y2); };
 Centi.prototype.line = function(_x1, _y1, _x2, _y2){
     this.ctx.beginPath();
     this.ctx.moveTo(_x1, _y1);
     this.ctx.lineTo(_x2, _y2);
     this.ctx.stroke();
-}
+};
+
+Centi.prototype.moveTo = function(_x1, _y1){
+    this.ctx.moveTo(_x1, _y1);
+};
+
+Centi.prototype.lineTo = function(_x1, _y1){
+    this.ctx.lineTo(_x1, _y1);
+};
 
 Centi.prototype.oval = function(_x, _y, _rad) {
     this.ctx.beginPath();
     this.ctx.arc(_x, _y, _rad, 0, Math.PI * 2, true);
     if ( this.bFill ) this.ctx.fill();
     else this.ctx.stroke()
-}
+};
+
+Centi.prototype.beginShape = function(){
+    this.ctx.beginPath();
+};
+
+Centi.prototype.endShape = function(){
+    if ( this.bFill ) this.ctx.fill();
+    else this.ctx.stroke();
+};
+
+Centi.prototype.drawMe = function(){
+    var len = arguments.length;
+    if ( len == 2 ) {
+        this.ctx.drawImage(this.canvas, arguments[0], arguments[1]);
+    } else if ( len == 4 ) {
+        this.ctx.drawImage(this.canvas, arguments[0], arguments[1], arguments[2], arguments[3]);
+    } else if ( len == 8 ) {
+        this.ctx.drawImage(this.canvas, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7]);   
+    }
+};
 
 // transform
 
@@ -263,35 +293,49 @@ Centi.prototype.col = function(){
     }
     this.ctx.fillStyle = s;
     this.ctx.strokeStyle = s;
-}
+};
 
 Centi.prototype.fill = function(){
     this.bFill = true;
-}
+};
 
-Centi.prototype.strk = function(){ this.stroke(); }
+Centi.prototype.strk = function(){ this.stroke(); };
 Centi.prototype.stroke = function(){
     this.bFill = false;
-}
+};
 
 Centi.prototype.interp = function(a, b, rate){
     return b + (a-b)*rate;
+};
+
+Centi.prototype.sortNum = function(_arr){
+    _arr.sort(
+        function(a,b){
+            if( a < b ) return -1;
+            if( a > b ) return 1;
+            return 0;
+        }
+    );
+}
+
+Centi.prototype.reverse = function(_arr){
+    _arr.reverse();
 }
 
 Centi.prototype.vec2 = function(_x, _y){
     return new Centi.Vec2(_x, _y);
-}
+};
 Centi.Vec2 = function(_x, _y){
     this.x = _x;
     this.y = _y;
-};
+};;
 
 // centi funcs
 
 Centi.prototype.reset = function(){
     this.c = 0;
     this.bFill = true;
-}
+};
 
 var CT_FUNCS = Object.getOwnPropertyNames(Centi.prototype);
 
